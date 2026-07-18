eff938424c6087cb1c082a9ae281560761eca578
jeremiasavaro <jeremiasavaro7@gmail.com>
2026-07-18 15:51:20 -0300

    Organize imports alphabetic

--- app/tests/fixtures/__init__.py
+++ app/tests/fixtures/__init__.py
@@ -2,7 +2,7 @@ from tests.fixtures.auth import auth_headers, registered_user
 from tests.fixtures.client import client
 
 __all__ = [
+    "auth_headers",
     "client",
     "registered_user",
-    "auth_headers",
 ]
